899978f6fb03bfd5f59836ab5a49cdef4328c636
Alan Silva <alanfranciscos@gmail.com>
2023-05-11 20:04:04 -0300

    Initialize api project

---
+++ src/test/java/br/com/desafiox/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiox.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
